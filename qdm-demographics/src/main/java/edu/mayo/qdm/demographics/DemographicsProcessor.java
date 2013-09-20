f69d9e761d4480d330cc8d5f280920ab19dd1132
Dale Suesse <suesse.dale@mayo.edu>
2013-09-20 14:27:54 -0500

    Fixes improper demographic items formatting. Condenses resulting xml.

--- qdm-demographics/src/main/java/edu/mayo/qdm/demographics/DemographicsProcessor.java
+++ qdm-demographics/src/main/java/edu/mayo/qdm/demographics/DemographicsProcessor.java
@@ -11,8 +11,10 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.StringWriter;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -21,12 +23,12 @@ import java.util.Set;
 public class DemographicsProcessor {
 
     public Demographics getDemographics(Map<String,Set<Patient>> results){
-        Map<AbstractDemographicsItem,Integer> counter = new HashMap<AbstractDemographicsItem,Integer>();
+        Map<AbstractDemographicsItem,Integer> counter = new HashMap<>();
 
         for(Map.Entry<String, Set<Patient>> entrySet : results.entrySet()){
             for(Patient patient : entrySet.getValue()){
                 Set<AbstractDemographicsItem> items =
-                    this.getDemographicItems(entrySet.getKey(), patient);
+                  this.getDemographicItems(entrySet.getKey(), patient);
 
                 for(AbstractDemographicsItem item : items){
                     if(! counter.containsKey(item)){
@@ -40,24 +42,67 @@ public class DemographicsProcessor {
         }
 
         Demographics demographics = new Demographics();
+        List<DemographicType> types = new ArrayList<>();
 
         for(Map.Entry<AbstractDemographicsItem, Integer> entrySet : counter.entrySet()){
             AbstractDemographicsItem item = entrySet.getKey();
 
-            DemographicStat stat = new DemographicStat();
-            stat.setLabel(item.getLabel());
+            DemographicType type = getType(types, item);
+            DemographicCategory category = getCategory(type, item);
+            DemographicStat stat = getStat(category, item);
             stat.setValue(BigInteger.valueOf(entrySet.getValue()));
-
-            DemographicCategory category = new DemographicCategory();
             category.getDemographicStat().add(stat);
+            addCategoryToType(type, category);
+        }
 
-            DemographicType type = new DemographicType();
-            type.getDemographicCategory().add(category);
-
+        for (DemographicType type : types) {
             demographics.getDemographicType().add(type);
         }
 
         return demographics;
+    }
+
+    private DemographicType getType(List<DemographicType> types, AbstractDemographicsItem item) {
+        for (DemographicType dt : types) {
+            if (dt.getType().equals(item.getPopulation())) {
+                return dt;
+            }
+        }
+        DemographicType dt = new DemographicType();
+        dt.setType(item.getPopulation());
+        types.add(dt);
+        return dt;
+    }
+
+    private DemographicCategory getCategory(DemographicType type, AbstractDemographicsItem item) {
+        for (DemographicCategory dc : type.getDemographicCategory()) {
+            if (dc.getName().equals(item.getStatistic())) {
+                return dc;
+            }
+        }
+        DemographicCategory dc = new DemographicCategory();
+        dc.setName(item.getStatistic());
+        return dc;
+    }
+
+    private DemographicStat getStat(DemographicCategory category, AbstractDemographicsItem item) {
+        for (DemographicStat stat : category.getDemographicStat()) {
+            if (stat.getLabel().equals(item.getLabel())) {
+                return stat;
+            }
+        }
+        DemographicStat stat = new DemographicStat();
+        stat.setLabel(item.getLabel());
+        return stat;
+    }
+
+    private void addCategoryToType(DemographicType type, DemographicCategory category) {
+        for (DemographicCategory dc : type.getDemographicCategory()) {
+            if (dc.getName().equals(category.getName())) {
+                return;
+            }
+        }
+        type.getDemographicCategory().add(category);
     }
 
     public String toXml(Demographics demographics) {
