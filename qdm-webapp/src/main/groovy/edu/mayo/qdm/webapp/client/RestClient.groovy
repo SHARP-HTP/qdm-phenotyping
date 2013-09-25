75f10d770f54b48de0a7f3f637d350ca0aefdd65
Kevin Peterson <kevin.peterson@mayo.edu>
2013-09-25 14:31:26 -0500

    Updates for self-signed SSL on REST client.

--- qdm-webapp/src/main/groovy/edu/mayo/qdm/webapp/client/RestClient.groovy
+++ qdm-webapp/src/main/groovy/edu/mayo/qdm/webapp/client/RestClient.groovy
@@ -1,12 +1,26 @@
 package edu.mayo.qdm.webapp.client
+
+import edu.mayo.qdm.webapp.rest.client.SelfSignedSSLSocketFactory
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.Method
-import static groovyx.net.http.ContentType.*
+import org.apache.http.conn.scheme.Scheme
+
+import java.security.KeyStore
+
+import static groovyx.net.http.ContentType.JSON
 
 class RestClient {
 
     def GET(url, accept="application/json", contentType=JSON, parse=true, params=null){
         def http = new HTTPBuilder(url)
+
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        trustStore.load(null, null);
+
+        def sslsf = new SelfSignedSSLSocketFactory(trustStore);
+
+        http.client.connectionManager.schemeRegistry.register(
+                new Scheme("https", sslsf, 443))
 
         def response = http.request(Method.GET, contentType ) {
             uri.query = params
